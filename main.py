bb69bbe0929f56fb7929ff906546237bfec32c1d
Ryan Hamilton <rhamil4@gmu.edu>
2023-11-28 20:49:21 -0500

    Added progress bar to frame capture

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def throw(problem):
     print(problem)
     exit(1)
 
-def print_progress_bar(iteration, total):
+def print_progress_bar(title, iteration, total):
     percent = "{:.1f}".format(100 * (iteration / float(total)))
-    progress = f"Progress: {iteration}/{total} - {percent}%"
+    progress = f"{title}: {iteration}/{total} - {percent}%"
     print(progress, end='\r', flush=True)
 
 def capture_frames(frames):
@@ -23,11 +23,17 @@ def capture_frames(frames):
     fps = video.get(cv2.CAP_PROP_FPS)
 
     video.set(cv2.CAP_PROP_POS_MSEC, startms)
+    pos_after_offset = int(video.get(cv2.CAP_PROP_POS_FRAMES))
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) - pos_after_offset
 
     while True:
+        frame_num = int(video.get(cv2.CAP_PROP_POS_FRAMES)) - pos_after_offset
+        print_progress_bar(" Extract Frames", frame_num, total_frames)
+
         ret, frame = video.read()
         if not ret: break
         frames.append(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
+    print()
 
     video.release()
 
@@ -41,16 +47,13 @@ if '__main__' == __name__:
     startms = int(args.startms or 0)
     num_columns = int(args.columns)
 
-    print("Extracting pngs from video " + file + " . . .")
-
     frames = []
     fps = capture_frames(frames)
     mspf = 1000 / fps
     num_frames = len(frames)
 
-    print('Generating ASCII art')
     for i in range(num_frames):
-        print_progress_bar(i, num_frames)
+        print_progress_bar(" ASCIIify", i + 1, num_frames)
         frame = frames[i]
 
         # https://www.desmos.com/calculator/zi1oxgc0xx
@@ -59,6 +62,7 @@ if '__main__' == __name__:
         tile_height = frame.height / num_rows
 
         convert_to_ascii.convert(i, frame, mspf, num_columns, num_rows, tile_width, tile_height)
+    print()
 
     # finalize .ytt and write to output
     convert_to_ascii.export(file[:file.rfind(".")])
